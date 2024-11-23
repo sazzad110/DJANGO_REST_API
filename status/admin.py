7bb9af4ff5513b73f426ff426f3a148696131e69
sazzad110 <sazzadhasan610@gmail.com>
2024-11-23 10:08:33 +0600

    create another status model in admin site

--- status/admin.py
+++ status/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from status.models import Status
 
 # Register your models here.
+class StatusAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Status, StatusAdmin)
