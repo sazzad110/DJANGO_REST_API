3dbde1e1c20ee8116e4c1911b0aff7043d981785
sazzad110 <sazzadhasan610@gmail.com>
2024-11-24 18:19:37 +0600

    Create another serializer

--- status/serializers.py
+++ status/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
 from status.models import Status
+from django.contrib.auth.models import User
+
+class UserSerializer(serializers.ModelSerializer):  # define new user serializer
+    class Meta:
+        model = User
+        fields = ['id','username','email',]
 
 class StatusSerializer(serializers.ModelSerializer):
+    user = UserSerializer(many=False)       # set 'user' defination here 
     class Meta:
         model = Status
         fields = ['id','text','created_at','user']      # id is default field in each model but hidden
